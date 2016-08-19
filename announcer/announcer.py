40c13a4530c16bc7fc43d81d4df76795219f90df
nigori <titan@live.com>
2016-08-19 08:17:00 -0700

    well, bot will join the channel now, but then crash.

--- announcer/announcer.py
+++ announcer/announcer.py
@@ -6,71 +6,87 @@ from .utils import checks
 import datetime
 from gtts import gTTS
 
+__author__ = 'nigori'
+__version__ = '0.0.1'
+
 class Announcer:
 
-    """Announcer users. The more I stare at this word (Announcer) the less it means."""
+    """Announcer users. Attempt to get the bot to join a channel, then play an MP3."""
 
     def __init__(self, bot):
         self.bot = bot
-        self.data_file = "data/announcer/Announcer.json"
         self.data_directory = "data/announcer/"
+        self.data = None
 
         try:
-            self.data = fileIO(data_file,"load")
+            self.data = fileIO("data/announcer/announcer.json","load")
         except:
-            print("Failure to load voice files. Creating new blank data file.")
-            self.data = {}
-            save()
+            save(self)
 
-    async def on_voice_state_update(self, member):
+    async def on_voice_state_update(self, before, member):
         if member.voice_channel:
-            # TODO: get the bot to join the same voice channel
-            voice_file = get_user_voice_file(member)
+            voice_file = self.get_user_voice_file(member)
+
+            print("got voice file:" + voice_file)
+            print(member.name)
+            print("self.data: " + str(self.data))
 
             # get bot to join proper voice channel
-            self.bot.join_voice_channel(member.voice_channel)
+            await self.bot.join_voice_channel(member.voice_channel)
 
+            self.speak_my_child(self, member)
+
+    async def speak_my_child(self, member):
             voice_client = self.bot.voice_client_in(member.server)
 
-            try:
-                voice_client.audio_player.process.kill()
-            except AttributeError:
-                pass
-            except ProcessLookupError:
-                pass
+            if voice_client:
+                try:
+                    voice_client.audio_player.process.kill()
+                except AttributeError:
+                    pass
+                except ProcessLookupError:
+                    pass
 
-            voice_client.create_ffmpeg_player(voice_file, False, options='-b:a 64k -bufsize 64k')
-            voice_client.audio_player.volume = self.get_server_settings(member.server)['VOLUME'] / 100
-            voice_client.audio_player.start()
+                voice_client.create_ffmpeg_player(voice_file, False, options='-b:a 64k -bufsize 64k')
+                voice_client.audio_player.volume = self.get_server_settings(member.server)['VOLUME'] / 100
+                await voice_client.audio_player.start()
 
 
     def get_user_voice_file(self, member):
         # load voice file for the user that joined
-        if not os.path.isfile(self.data_directory + member.name + ".mp3"):
-            voice_file = create_user_voice_file(member)
-            self.data[member.name] = voice_file
-            save()
-            return voice_file
 
+        print("attempting to get user voice file for ", member.name)
+
+        if not os.path.isfile(self.data_directory + member.name + ".mp3"):
+            voice_file = self.create_user_voice_file(member)
+            self.data[member.name] = voice_file
+            save(self)
+            return voice_file
+        elif not member.name in self.data:
+            self.data[member.name] = self.data_directory + member.name + ".mp3"
+            save(self)
         else:
             voice_file = self.data[member.name]
             return voice_file
 
     def create_user_voice_file(self, member):
+        print("attempting to create user voice file for ", member.name)
         name = member.name
         voice_audio = gTTS(text=member.name, lang='en')
         voice_file = self.data_directory + member.name + ".mp3"
         voice_audio.save(voice_file)
         return voice_file
 
-def save():
+def save(self):
     try:
-        fileIO(self.data_file, "save", self.data)
+        fileIO("data/announcer/announcer.json", "save", self.data)
     except:
-        print("Error saving! This shouldn't happen.")
+        print("saving an fresh announcer.json")
+        fileIO("data/announcer/announcer.json", "save", {})
+
 
 def build_folders():
-    folders = ("data", "data/Announcer/")
+    folders = ("data", "data/announcer/")
     for folder in folders:
         if not os.path.exists(folder):
             print("Creating " + folder + " folder...")
