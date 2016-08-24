20fe6dccf10904e876e2693957902388d5c4f7b6
nigori <titan@live.com>
2016-08-23 21:01:49 -0700

    conversion of print to logger, minor fixes

--- announcer/announcer.py
+++ announcer/announcer.py
@@ -3,6 +3,9 @@ from .utils.dataIO import fileIO
 from gtts import gTTS
 import asyncio
 import discord
+import logging
+
+log = logging.getLogger(__name__)
 
 __author__ = 'nigori'
 __version__ = '0.0.1'
@@ -23,46 +26,50 @@ class Announcer:
 
     async def on_voice_state_update(self, before, member):
         # bot should not announce itself when joining a channel
-        print("Grabbed User:%s with id:%s, and the bot id:%s" %
+        log.debug("Grabbed User:%s with id:%s, and the bot id:%s" %
               (member.name, member.id, self.bot.user.id))
         if member.id == self.bot.user.id:
             return
+        # add case where if current bot voice channel and member voice 
+        # channel are the same go right to speaking the name
         elif member.voice_channel:
             voice_file = self.get_user_voice_file(member)
             if not voice_file:
-                print("failed to get voice file for {}".format(member.name))
+                log.debug("failed to get voice file for {}".format(member.name))
                 return
-            print("got voice file:" + voice_file)
-            print(member.name)
-            print("self.data: " + str(self.data))
+            log.debug("got voice file:" + voice_file)
+            log.debug(member.name)
+            log.debug("self.data: " + str(self.data))
 
             # this next join_voice_channel appears to be causing the timeout
             try:
                 await self.bot.join_voice_channel(member.voice_channel)
             except discord.opus.OpusNotLoaded:
-                print("OPUS NO LOADED EXCEPTION!!")
-                await self.bot.say("Opus is not loaded! Will not work.")
+                log.debug("OPUS NO LOADED EXCEPTION!!")
 
-            print("PAST JOINING CHANNEL. PRIOR TO SPEAK")
+            log.debug("PAST JOINING CHANNEL. PRIOR TO SPEAK")
             # attempt to speak the TTS name in channel
             await self.speak_my_child(self, member, voice_file)
 
     async def speak_my_child(self, member, voice_file):
-            print("ATTEMPTING TO MAKE THIS BEAST SPEAK")
+            log.debug("ATTEMPTING TO MAKE THIS BEAST SPEAK")
             voice_client = self.bot.voice_client_in(member.server)
 
             if voice_client:
-                voice_client.create_ffmpeg_player(voice_file, False, options='-b:a 64k -bufsize 64k')
-                voice_client.audio_player.volume = self.get_server_settings(member.server)['VOLUME'] / 100
+                voice_client.audio_player = \
+                    voice_client.create_ffmpeg_player(
+                        voice_file, False, options='-b:a 64k -bufsize 64k')
+                voice_client.audio_player.volume = \
+                    self.get_server_settings(member.server)['VOLUME'] / 100
                 await voice_client.audio_player.start()
                 await asyncio.sleep(6)
                 await voice_client.audio_player.stop()
             else:
-                print("in speakmychild, don't have a handle to voice client.")
+                log.debug("in speakmychild, don't have a handle to voice client.")
 
     def get_user_voice_file(self, member):
         # load voice file for the user that joined
-        print("attempting to get user voice file for ", member.name)
+        log.debug("attempting to get user voice file for ", member.name)
 
         if not os.path.isfile(self.data_directory + member.name + ".mp3"):
             voice_file = self.create_user_voice_file(member)
@@ -77,7 +84,7 @@ class Announcer:
             return voice_file
 
     def create_user_voice_file(self, member):
-        print("attempting to create user voice file for ", member.name)
+        log.debug("attempting to create user voice file for ", member.name)
         voice_audio = gTTS(text=member.name, lang='en')
         voice_file = self.data_directory + member.name + ".mp3"
         voice_audio.save(voice_file)
@@ -88,7 +95,7 @@ def save(self):
     try:
         fileIO("data/announcer/announcer.json", "save", self.data)
     except:
-        print("saving an fresh announcer.json")
+        log.debug("saving an fresh announcer.json")
         fileIO("data/announcer/announcer.json", "save", {})
 
 
@@ -96,7 +103,7 @@ def build_folders():
     folders = ("data", "data/announcer/")
     for folder in folders:
         if not os.path.exists(folder):
-            print("Creating " + folder + " folder...")
+            log.debug("Creating " + folder + " folder...")
             os.makedirs(folder)
 
 
